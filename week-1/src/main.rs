edf5fb690f620eda601b72c90a0f00a34e2ad65a
bowobudiprasetyo <bowo@thebeam.co>
2025-12-04 17:42:29 +0700

    initial commit

---
+++ week-1/src/main.rs
@@ -0,0 +1,59 @@
+
+fn main() {
+    println!("Hello, world!");
+    // let number = 121;
+    // let result = is_palindrome(number);
+    // println!("result is {}", result);
+
+    let result = calculator(5, 2, "$");
+    let result2=  match result {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
+
+    println!("Result2: {}", result2);
+
+
+
+}
+
+fn is_palindrome(num: i32) -> bool {
+    let chars: Vec<char> = num.to_string().chars().collect();
+    let mut right = chars.len() -1;
+    let mut left = 0 ;
+
+    while left < right {
+        if chars[left] != chars[right] {
+            return false;
+        }
+        right -= 1;
+        left += 1;
+    }
+    return true
+}
+
+fn calculator(
+    num1: i32,
+    num2: i32,
+    operator: &str,
+) -> Result<i32, String> {
+    match operator {
+        "+" => Ok(num1 + num2),
+        "-" => Ok(num1 - num2),
+        "*" => Ok(num1 * num2),
+        "/" =>{
+            if num2 == 0 {
+                Err("Divide by zero".to_string())
+            }else {
+                Ok(num1 / num2)
+            }
+
+        },
+        _ => Err(format!("Unknown operator: {}", operator))
+
+    }
+
+}
